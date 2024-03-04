35bf66e6844b7774ec9a0384e40689b67f779307
Robbie Paul <robbiepaul@me.com>
2024-03-04 21:44:35 +0000

    Validate phone number

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,4 +2,11 @@ class Applicant < ApplicationRecord
   belongs_to :property
 
   validates :first_name, :last_name, :email, :phone, presence: true
+  validate :phone_number_valid?
+
+  def phone_number_valid?
+    return if phone.blank?
+
+    errors.add(:phone, 'must be a valid phone number') unless phone.match?(/\A(\+44|0)\d{10}\z/)
+  end
 end
