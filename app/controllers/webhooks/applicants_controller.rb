cb37694115fe64e0ea1c6f7466ad91bf5451fc0e
Robbie Paul <robbiepaul@me.com>
2024-03-04 21:11:36 +0000

    Add basic webhook controller

---
+++ app/controllers/webhooks/applicants_controller.rb
@@ -0,0 +1,7 @@
+module Webhooks
+  class ApplicantsController < ApplicationController
+    def import
+      # Your code for importing applicants goes here
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :webhooks do
+    post "import_applicants" => "applicants#import"
+  end
 end
