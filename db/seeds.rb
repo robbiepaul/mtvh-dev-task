327b618027f42d932e2a7375bf56ad8ced787454
Robbie Paul <robbiepaul@me.com>
2024-03-05 15:11:24 +0000

    Load the properties fixtures in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ENV['FIXTURES_PATH'] = 'spec/fixtures'
+
+Rake.application["db:fixtures:load"].invoke
